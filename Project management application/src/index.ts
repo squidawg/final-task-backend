75dd7b710f87972fe1b276e2d125be3e9af1d397
squidawg <78506807+squidawg@users.noreply.github.com>
2023-05-04 12:53:26 +0400

    refactor: change path

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -3,7 +3,6 @@ import { PORT } from './constants';
 
 import * as serverService from './services/server.service';
 
-
 (async () => {
   try {
     await mongoose.connect('mongodb+srv://alexkronin:j4VYHz62v4JWNCK6@cluster0.vajrxco.mongodb.net/?retryWrites=true&w=majority');
