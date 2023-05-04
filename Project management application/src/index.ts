7eb76ded2ab0ceeee05700dd6a9521371ce95927
squidawg <78506807+squidawg@users.noreply.github.com>
2023-05-04 12:28:53 +0400

    fix : fix5

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -6,7 +6,7 @@ import * as serverService from './services/server.service';
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://alexkronin:j4VYHz62v4JWNCK6@cluster0.vajrxco.mongodb.net/managerApp');
+    await mongoose.connect('mongodb+srv://alexkronin:j4VYHz62v4JWNCK6@cluster0.vajrxco.mongodb.net/?retryWrites=true&w=majority');
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
