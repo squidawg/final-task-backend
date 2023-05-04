6f217e2bf18e6271c3a978b18e6722672c67b257
squidawg <78506807+squidawg@users.noreply.github.com>
2023-05-04 15:27:18 +0400

    Update index.ts

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 import { PORT } from './constants';
 
 import * as serverService from './services/server.service';
+
 (async () => {
   try {
     await mongoose.connect('mongodb+srv://alexkronin:j4VYHz62v4JWNCK6@cluster0.vajrxco.mongodb.net/?retryWrites=true&w=majority');
