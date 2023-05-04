436a3afbf79ee395a456ad27b69c53d8c9e80ffe
squidawg <78506807+squidawg@users.noreply.github.com>
2023-05-04 13:03:25 +0400

    refactor: change mongoose string

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -5,7 +5,7 @@ import * as serverService from './services/server.service';
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://alexkronin:j4VYHz62v4JWNCK6@cluster0.vajrxco.mongodb.net/?retryWrites=true&w=majority');
+    await mongoose.connect('mongodb+srv://alexkronin:j4VYHz62v4JWNCK6@cluster0.vajrxco.mongodb.net/managerApp');
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
