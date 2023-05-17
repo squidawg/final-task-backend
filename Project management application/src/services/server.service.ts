bb6c703fdb5c8b00e11011c2bd9f0c65228f6c07
squidawg <78506807+squidawg@users.noreply.github.com>
2023-05-17 19:23:35 +0400

    fix:CORS

--- Project management application/src/services/server.service.ts
+++ Project management application/src/services/server.service.ts
@@ -31,7 +31,7 @@ app.use(
 );
 app.use(cors({
   origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 app.use(mung);
